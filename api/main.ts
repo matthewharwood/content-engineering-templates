231d201a6ab7871bc5c5509b5cb4438460fa524b
matthew harwood <matthhar12@gmail.com>
2025-09-26 21:38:09 -0700

    github action

--- api/main.ts
+++ api/main.ts
@@ -1,24 +1,10 @@
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
-
 Deno.serve(async (req) => {
   const url = new URL(req.url);
 
-  // API endpoint
   if (url.pathname === "/api/hello") {
     return new Response("Hello, world!");
   }
 
-  // Serve Storybook from /storybook path
-  if (url.pathname.startsWith("/storybook")) {
-    const path = url.pathname.replace("/storybook", "");
-    return serveDir(req, {
-      fsRoot: "./ui/storybook-static",
-      urlRoot: "storybook",
-      quiet: true,
-    });
-  }
-
-  // Serve main index.html for root
   if (url.pathname === "/") {
     try {
       const html = await Deno.readTextFile("./index.html");
