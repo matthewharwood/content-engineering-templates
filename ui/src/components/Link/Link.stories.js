c19cedf8ddeb90806204fb52e5c5b261379ef40d
Matthew Harwood <matthhar12@gmail.com>
2025-10-04 14:49:03 -0700

    Add Link component styles and story examples
    Introduces Link.css with comprehensive styling for link variants, sizes, icons, and external indicators. Adds Storybook stories and HTML examples demonstrating all link variants, pseudo-states, icon support, and use cases for documentation and UI showcase.

---
+++ ui/src/components/Link/Link.stories.js
@@ -0,0 +1,62 @@
+import './Link.css';
+import LinkPrimaryHTML from './LinkPrimary.html?raw';
+import LinkSecondaryHTML from './LinkSecondary.html?raw';
+import LinkOnSurfaceHTML from './LinkOnSurface.html?raw';
+import LinkInlineHTML from './LinkInline.html?raw';
+import LinkNoUnderlineHTML from './LinkNoUnderline.html?raw';
+import LinkSizesHTML from './LinkSizes.html?raw';
+import LinkWithIconsHTML from './LinkWithIcons.html?raw';
+import LinkExternalHTML from './LinkExternal.html?raw';
+import LinkPseudoStatesHTML from './LinkPseudoStates.html?raw';
+import LinkShowcaseHTML from './LinkShowcase.html?raw';
+
+function createHTMLElement(html) {
+  const container = document.createElement('div');
+  container.innerHTML = html;
+  return container;
+}
+
+export default {
+  title: 'Components/Link',
+  tags: ['autodocs'],
+};
+
+export const Primary = {
+  render: () => createHTMLElement(LinkPrimaryHTML)
+};
+
+export const Secondary = {
+  render: () => createHTMLElement(LinkSecondaryHTML)
+};
+
+export const OnSurface = {
+  render: () => createHTMLElement(LinkOnSurfaceHTML)
+};
+
+export const Inline = {
+  render: () => createHTMLElement(LinkInlineHTML)
+};
+
+export const NoUnderline = {
+  render: () => createHTMLElement(LinkNoUnderlineHTML)
+};
+
+export const Sizes = {
+  render: () => createHTMLElement(LinkSizesHTML)
+};
+
+export const WithIcons = {
+  render: () => createHTMLElement(LinkWithIconsHTML)
+};
+
+export const External = {
+  render: () => createHTMLElement(LinkExternalHTML)
+};
+
+export const PseudoStates = {
+  render: () => createHTMLElement(LinkPseudoStatesHTML)
+};
+
+export const Showcase = {
+  render: () => createHTMLElement(LinkShowcaseHTML)
+};
